526fe343011a0246168c96881f7c8c2fff0f9aa0
chenyue404 <601450225@qq.com>
2024-02-27 15:24:50 +0800

    写入剪贴板之前先判断是否相同

--- clipboard_listener.py
+++ clipboard_listener.py
@@ -15,11 +15,7 @@ file_path = "config.json"
 config = {}
 if not os.path.exists(file_path):
     with open(file_path, "w") as f:
-            json.dump({
-                "clientName": "",
-                "url": "",
-                "token": ""
-            }, f)
+        json.dump({"clientName": "", "url": "", "token": ""}, f)
 if os.path.exists(file_path):
     with open(file_path, "r") as f:
         config = json.load(f)
@@ -47,46 +43,62 @@ if len(TOKEN) == 0:
 with open(file_path, "w") as f:
     json.dump(config, f, indent=4)
 
+
 def on_message(ws, message):
-    print(message)
-    jo = json.loads(message)
-    title = jo.get("title")
-    msg = jo.get("message")
-    if msg is None:
-        return 
-    if title != CLIENTNAME:
-        print("收到：%s" % (msg))
-        clipboard.copy(msg)
+    print(f"on_message: {message}")
+    data = json.loads(message)
+    title = data.get("title")
+    message_content = data.get("message")
+
+    if not message_content:
+        return
+
+    # 判断消息是否来自自己
+    if title == CLIENTNAME:
+        return
+
+    # 判断消息内容是否与剪贴板内容一致
+    current_clipboard_content = clipboard.paste()
+    if message_content == current_clipboard_content:
+        return
+
+    print(f"收到: {message_content}")
+    clipboard.copy(message_content)
+
 
 def on_error(ws, error):
     print(error)
 
+
 def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
+    print(f"### closed ###:\n close_status_code={close_status_code}\n close_msg={close_msg}")
+
 
 def on_open(ws):
     print("Opened connection")
 
+
 print("wss://%s/ws" % (URL))
 # 创建websocket连接
-ws = websocket.WebSocketApp("wss://%s/ws" % (URL),
-                            header={
-                                "Authorization": TOKEN if TOKEN != "-" else ""
-                            },
-                             on_message=on_message,
-                             on_error=on_error,
-                             on_close=on_close,
-                             on_open=on_open)
+ws = websocket.WebSocketApp(
+    "wss://%s/ws" % (URL),
+    header={"Authorization": TOKEN if TOKEN != "-" else ""},
+    on_message=on_message,
+    on_error=on_error,
+    on_close=on_close,
+    on_open=on_open)
 wsThread = threading.Thread(target=ws.run_forever, daemon=True)
 wsThread.start()
 
+
 def send_clipboard(msg):
     requests.post("https://%s" % (URL),
-                  data=msg,
+                  data=msg.encode(encoding='utf-8'),
                   headers={
-                    "Authorization": TOKEN if TOKEN != "-" else "",
-                    "Title": CLIENTNAME
-                    })
+                      "Authorization": TOKEN if TOKEN != "-" else "",
+                      "Title": CLIENTNAME
+                  })
+
 
 def listen_clipboard():
     oldText = clipboard.paste()
@@ -94,29 +106,33 @@ def listen_clipboard():
     while True:
         # 读取剪贴板的内容
         text = clipboard.paste()
-    
+
         # 如果剪贴板有内容，则打印内容
         if (text != oldText):
             print("剪贴板变化：%s" % (text))
             oldText = text
             send_clipboard(text)
-    
+
         # 延迟1秒钟
         time.sleep(1)
+
 
 listenThread = threading.Thread(target=listen_clipboard, daemon=True)
 listenThread.start()
 
 print("Enter 'exit' to quit: ")
 print("Connecting……")
+
+
 def input_read():
     while True:
         user_input = input()
         if user_input == "exit":
-           ws.close()
-           break
+            ws.close()
+            break
         else:
             print("Enter 'exit' to quit: ")
+
 
 # thread2 = threading.Thread(target=input_read, daemon=True)
 # thread2.start()
